f141740e9c49a386fb9f3ca470b786e13706025c
ValPare <valentin.pare@gmail.com>
2015-12-03 08:30:45 +0100

    #test push

--- Java-projet/projet-formulaire/src/test/java/formation/service/UserServiceTest.java
+++ Java-projet/projet-formulaire/src/test/java/formation/service/UserServiceTest.java
@@ -22,6 +22,7 @@ public class UserServiceTest {
     // ======================================
     // =             Attributes             =
     // ======================================
+    //test
 
     protected static Context ctx;
 
